01adc4059c36b6fc0289ebc0377848376a94698b
Tom Liang <tomliangg@gmail.com>
2019-02-06 10:45:20 -0800

    API test

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <Arduino.h>
 
 #include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <ArduinoJson.h>
 
 // Replace these with your WiFi network settings
 const char* ssid = "Tom_Asus"; //replace this with your WiFi network name
@@ -27,5 +29,32 @@ void setup()
 }
 
 void loop() {
-
+  // Check WiFi Status
+  if (WiFi.status() == WL_CONNECTED) {
+    HTTPClient http;  //Object of class HTTPClient
+    http.begin("http://jsonplaceholder.typicode.com/users/1");
+    int httpCode = http.GET();
+    //Check the returning code                                                                  
+    if (httpCode > 0) {
+      // Parsing
+      const size_t bufferSize = JSON_OBJECT_SIZE(2) + JSON_OBJECT_SIZE(3) + JSON_OBJECT_SIZE(5) + JSON_OBJECT_SIZE(8) + 370;
+      DynamicJsonBuffer jsonBuffer(bufferSize);
+      JsonObject& root = jsonBuffer.parseObject(http.getString());
+      // Parameters
+      int id = root["id"]; // 1
+      const char* name = root["name"]; // "Leanne Graham"
+      const char* username = root["username"]; // "Bret"
+      const char* email = root["email"]; // "Sincere@april.biz"
+      // Output to serial monitor
+      Serial.print("Name:");
+      Serial.println(name);
+      Serial.print("Username:");
+      Serial.println(username);
+      Serial.print("Email:"); 
+      Serial.println(email);
+    }
+    http.end();   //Close connection
+  }
+  // Delay
+  delay(60000);
 }
