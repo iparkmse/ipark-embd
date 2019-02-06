64bb526d7cb892c09130ae232ad51730d4a7c5db
Tom Liang <tomliangg@gmail.com>
2019-02-06 09:57:51 -0800

    wifi test

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,31 @@
 #include <Arduino.h>
 
-#define LED D0
+#include <ESP8266WiFi.h>
 
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(LED, OUTPUT);    // LED pin as output
+// Replace these with your WiFi network settings
+const char* ssid = "Tom_Asus"; //replace this with your WiFi network name
+const char* password = "12345678"; //replace this with your WiFi network password
+
+void setup()
+{
+  delay(1000);
+  Serial.begin(9600); // baud rate 9600 or 115200, depending on the firmware
+ 
+  WiFi.begin(ssid, password);
+
+  Serial.println();
+  Serial.print("Connecting");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("success!");
+  Serial.print("IP Address is: ");
+  Serial.println(WiFi.localIP());
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(LED, HIGH);// turn the LED off.(Note that LOW is the voltage level but actually 
-                        //the LED is on; this is because it is acive low on the ESP8266.
-  delay(1000);            // wait for 1 second.
-  digitalWrite(LED, LOW); // turn the LED on.
-  delay(1000); // wait for 1 second.
+
 }
