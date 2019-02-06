79578ed8e477689b31f65c9e2c31f23c966d7b6e
Tom Liang <tomliangg@gmail.com>
2019-02-06 09:43:10 -0800

    blinking test

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,17 @@
 #include <Arduino.h>
 
+#define LED D0
+
 void setup() {
   // put your setup code here, to run once:
+  pinMode(LED, OUTPUT);    // LED pin as output
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  digitalWrite(LED, HIGH);// turn the LED off.(Note that LOW is the voltage level but actually 
+                        //the LED is on; this is because it is acive low on the ESP8266.
+  delay(1000);            // wait for 1 second.
+  digitalWrite(LED, LOW); // turn the LED on.
+  delay(1000); // wait for 1 second.
 }
