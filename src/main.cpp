4fa446b053811dcf517880af7df36ce023cdc53c
firda wijaya <96firda@gmail.com>
2019-02-19 14:08:52 -0800

    Fetching and writing to firebase with built-in library

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,28 @@
 #include <Arduino.h>
-
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <ArduinoJson.h>
 
+//Include firebase library
+#include <Firebase.h>
+#include <FirebaseArduino.h>
+#include <FirebaseCloudMessaging.h>
+#include <FirebaseError.h>
+#include <FirebaseHttpClient.h>
+#include <FirebaseObject.h>
+
+#define FIREBASE_HOST "my-test1-0123.firebaseio.com"
+#define FIREBASE_AUTH "Lzzd3o6bTR0o59CtImDzm4MAULRJsnWwNlpzGDqu" //contains secret, needs not to be hardcoded in the future
+
 // Replace these with your WiFi network settings
-const char* ssid = "Tom_Asus"; //replace this with your WiFi network name
-const char* password = "12345678"; //replace this with your WiFi network password
+const char* ssid = "Firda's iPhone"; //replace this with WiFi network name
+const char* password = "firda0303"; //replace this with WiFi network password
 
 void setup()
 {
   delay(1000);
   Serial.begin(9600); // baud rate 9600 or 115200, depending on the firmware
- 
+
   WiFi.begin(ssid, password);
 
   Serial.println();
@@ -26,35 +36,42 @@ void setup()
   Serial.println("success!");
   Serial.print("IP Address is: ");
   Serial.println(WiFi.localIP());
+
+  //Firebase initialization
+  Firebase.begin(FIREBASE_HOST,FIREBASE_AUTH);
 }
 
 void loop() {
   // Check WiFi Status
   if (WiFi.status() == WL_CONNECTED) {
-    HTTPClient http;  //Object of class HTTPClient
-    http.begin("http://jsonplaceholder.typicode.com/users/1");
-    int httpCode = http.GET();
-    //Check the returning code                                                                  
-    if (httpCode > 0) {
-      // Parsing
-      const size_t bufferSize = JSON_OBJECT_SIZE(2) + JSON_OBJECT_SIZE(3) + JSON_OBJECT_SIZE(5) + JSON_OBJECT_SIZE(8) + 370;
-      DynamicJsonBuffer jsonBuffer(bufferSize);
-      JsonObject& root = jsonBuffer.parseObject(http.getString());
-      // Parameters
-      int id = root["id"]; // 1
-      const char* name = root["name"]; // "Leanne Graham"
-      const char* username = root["username"]; // "Bret"
-      const char* email = root["email"]; // "Sincere@april.biz"
-      // Output to serial monitor
-      Serial.print("Name:");
-      Serial.println(name);
-      Serial.print("Username:");
-      Serial.println(username);
-      Serial.print("Email:"); 
-      Serial.println(email);
+    // get value
+    Serial.print("Stall 1 status:");
+    String stall_status1 = Firebase.getString("stall1/status/");
+    Serial.println(stall_status1);
+
+    if (stall_status1="available"){
+      Serial.println("Available for reservation");
+      Firebase.setString("stall1/reservation/","yes");
     }
-    http.end();   //Close connection
+    else{
+      Serial.println("Stall is busy");
+      Firebase.setString("stall1/reservation/","no");
+    }
+
+    Serial.print("Stall 2 status:");
+    String stall_status2 = Firebase.getString("stall2/status/");
+    Serial.println(stall_status2);
+
+    if (stall_status2="available"){
+      Serial.print("Available for reservation");
+      Firebase.setString("stall2/reservation/","yes");
+    }
+    else{
+      Serial.print("Stall is busy");
+      Firebase.setString("stall2/reservation/","no");
+    }
+
   }
   // Delay
-  delay(60000);
+  delay(3000); //fetching data from firebase every 3 seconds
 }
