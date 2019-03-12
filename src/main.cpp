5d8e906801b3395abca7e77607296d63a1ae8eba
firda wijaya <96firda@gmail.com>
2019-03-12 09:08:29 -0700

    Update main code

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 #include <ESP8266HTTPClient.h>
 #include <ArduinoJson.h>
 
-//Include firebase library
+// Include firebase library
 #include <Firebase.h>
 #include <FirebaseArduino.h>
 #include <FirebaseCloudMessaging.h>
@@ -12,22 +12,21 @@
 #include <FirebaseObject.h>
 
 #define FIREBASE_HOST "ipark-2997b.firebaseio.com"
-#define FIREBASE_AUTH "xxx" //contains secret, needs not to be hardcoded in the future
+#define FIREBASE_AUTH "xxx"  // Contains secret
 
 // Replace these with your WiFi network settings
-const char* ssid = "xxx"; //replace this with WiFi network name
-const char* password = "xxx"; //replace this with WiFi network password
+const char* ssid = "xxx";  // Replace this with WiFi network name
+const char* password = "xxx";  // Replace this with WiFi network password
 
-void setup(){
+void setup() {
   delay(1000);
-  Serial.begin(9600); // baud rate 9600 or 115200, depending on the firmware
+  Serial.begin(9600);  // Baud rate 9600 or 115200, depending on the firmware
 
   WiFi.begin(ssid, password);
 
   Serial.println();
   Serial.print("Connecting");
-  while (WiFi.status() != WL_CONNECTED)
-  {
+  while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
@@ -36,36 +35,32 @@ void setup(){
   Serial.print("IP Address is: ");
   Serial.println(WiFi.localIP());
 
-  //Firebase initialization
-  Firebase.begin(FIREBASE_HOST,FIREBASE_AUTH);
+  // Firebase initialization
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
 }
 
 void loop() {
   // Check WiFi Status
   if (WiFi.status() == WL_CONNECTED) {
-    //Jack's code should reside inside here while using the function readingData or writingData.
-
+    // Jack's code should reside inside here while using the function.
   }
   // Delay
-  delay(3000); //fetching data from firebase every 3 seconds
+  delay(3000);  // Fetching data from firebase every 3 seconds
 }
 
-void readingData(String stallNumber){
+void readingData(String stallNumber) {
   // Reading stall status
   String stall_status = Firebase.getString(stallNumber + "/status/");
   Serial.println("Stall status is " + stall_status);
-
 }
 
-void writingData(String stallNumber , String status){
+void writingData(String stallNumber, String status) {
   // Writing stall status
-  if (status="occupied"){
+  if (status = "occupied") {
     Serial.print("Available for reservation ");
-    Firebase.setString("stall/"+stallNumber+"/status/",status);
-  }
-  else if(status="vacant"){
+    Firebase.setString("stall/"+stallNumber+"/status/", status);
+  } else if (status = "vacant") {
     Serial.print("Stall number "+stallNumber);
-    Firebase.setString("stall/"+stallNumber+"/status/",status);
+    Firebase.setString("stall/"+stallNumber+"/status/", status);
   }
-
 }
